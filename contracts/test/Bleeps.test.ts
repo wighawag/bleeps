93368c877e553af97428f8934ac519e296ac5590
wighawag <wighawag@gmail.com>
2021-10-23 22:42:28 +0100

    noise

--- contracts/test/Bleeps.test.ts
+++ contracts/test/Bleeps.test.ts
@@ -23,7 +23,7 @@ describe('Bleeps', function () {
   it('minting works', async function () {
     const {users, Bleeps} = await setup();
     const note = 0;
-    const instr = 7;
+    const instr = 6;
     const tokenID = note + instr * 64;
     await expect(users[0].Bleeps.mint(tokenID, users[0].address, {value: parseEther('3')}))
       .to.emit(Bleeps, 'Transfer')
