d08150b61f1515882772d95bf22cec22bf87a4c4
wighawag <wighawag@gmail.com>
2021-11-10 21:51:10 +0000

    deploy on rinkeby

--- contracts/deploy/001_bleeps/002_deploy_bleeps_initial_sale.ts
+++ contracts/deploy/001_bleeps/002_deploy_bleeps_initial_sale.ts
@@ -11,15 +11,31 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const MandalaToken = await deployments.get('MandalaToken');
   const Bleeps = await deployments.get('Bleeps');
 
+  // const BleepsInitialSale = await deploy('BleepsInitialSale', {
+  //   contract: 'BleepsDutchAuction',
+  //   from: deployer,
+  //   args: [
+  //     Bleeps.address,
+  //     parseEther('2'),
+  //     2 * 24 * 3600,
+  //     parseEther('0.1'),
+  //     Math.floor(Date.now() / 1000), // TODO double check
+  //     saleRecipient,
+  //     MandalaToken.address,
+  //     20, // 20% discount
+  //   ],
+  //   skipIfAlreadyDeployed: true,
+  //   log: true,
+  //   autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+  // });
+
   const BleepsInitialSale = await deploy('BleepsInitialSale', {
-    contract: 'BleepsDutchAuction',
+    contract: 'BleepsFixedPriceSale',
     from: deployer,
     args: [
       Bleeps.address,
-      parseEther('2'),
-      2 * 24 * 3600,
-      parseEther('0.1'),
-      Math.floor(Date.now() / 1000), // TODO double check
+      parseEther('0.2'),
+      '0x0000000000000000000000000000000000000000000000000000000000000000', // TODO merkletree
       saleRecipient,
       MandalaToken.address,
       20, // 20% discount
