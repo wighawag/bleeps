055b2892474ca8bc1c5cdbcb0639765e4a729be2
wighawag <wighawag@gmail.com>
2021-11-16 19:12:44 +0000

    mintBleeps

--- contracts/scripts/mintBleeps.ts
+++ contracts/scripts/mintBleeps.ts
@@ -1,18 +1,50 @@
-import {parseEther} from 'ethers/lib/utils';
-import {getUnnamedAccounts, deployments} from 'hardhat';
+import {joinSignature} from '@ethersproject/bytes';
+import {calculateHash, hashLeaves, MerkleTree} from 'bleeps-common';
+import {Wallet} from 'ethers';
+import {parseEther, SigningKey, solidityKeccak256} from 'ethers/lib/utils';
+import {getUnnamedAccounts, deployments, ethers, network} from 'hardhat';
+import {BleepsFixedPriceSale} from '../typechain';
+import fs from 'fs';
 const {execute} = deployments;
 
 const args = process.argv.slice(2);
+const passId = args[0];
+const tokenId = args[0];
 
 async function main() {
   const others = await getUnnamedAccounts();
-  await execute(
-    'BleepsInitialSale',
-    {from: others[0], value: parseEther('0.2'), log: true},
-    'mint',
-    args[0],
-    others[0]
-  );
+  const from = others[0];
+  const to = others[0];
+
+  const {linkedData} = await deployments.get('BleepsInitialSale');
+  const leaves = linkedData.leaves;
+  const tree = new MerkleTree(hashLeaves(leaves));
+
+  let privateKeys; // = //await deployments.loadDotFile()
+  try {
+    const dataStr = fs.readFileSync(`deployments/${network.name}/.privateKeys.json`).toString();
+    privateKeys = JSON.parse(dataStr);
+  } catch (e) {}
+  const signingKey = new SigningKey(privateKeys[passId]);
+  const signature = signingKey.signDigest(solidityKeccak256(['uint256', 'address'], [passId, to]));
+  const proof = tree.getProof(calculateHash(passId, new Wallet(signingKey.privateKey).address));
+  // proof[0] = proof[0].slice(0, 64) + 'FF'; // make it invalid
+
+  const BleepsInitialSale = <BleepsFixedPriceSale>await ethers.getContract('BleepsInitialSale', from);
+  const tx = await BleepsInitialSale.mintWithSalePass(tokenId, to, passId, joinSignature(signature), proof, {
+    value: parseEther('0.2'),
+  });
+  await tx.wait();
+
+  // await execute(
+  //   'BleepsInitialSale',
+  //   {from: others[0], value: parseEther('0.2'), log: true},
+  //   'mint',
+  //   args[0],
+  //   others[0]
+  // );
+
+  return tx;
 }
 
 main()
