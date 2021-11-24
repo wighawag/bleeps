81d512472c98a1705133378f1cd654dc05b34e1a
wighawag <wighawag@gmail.com>
2021-11-24 14:53:16 +0000

    staging

--- contracts/deploy/004_bleeps_setup/000_set_ens_name_for_bleeps.ts
+++ contracts/deploy/004_bleeps_setup/000_set_ens_name_for_bleeps.ts
@@ -1,5 +1,7 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
+// eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-explicit-any
+const namehash = require('eth-ens-namehash') as any;
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments} = hre;
@@ -10,10 +12,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const bleepsOwner = await read('Bleeps', 'owner');
 
   const ENS = await deployments.getOrNull('ENS');
+
   if (ENS) {
-    await execute('Bleeps', {from: bleepsOwner, log: true, autoMine: true}, 'setENSName', 'bleeps.eth');
+    const expectedNode = namehash.hash(`${Bleeps.address.toLowerCase().slice(2)}.addr.reverse`);
     const node = await read('ReverseRegistrar', 'node', Bleeps.address);
-    log({ens_reverse_node: node});
+    if (expectedNode !== node) {
+      await execute('Bleeps', {from: bleepsOwner, log: true, autoMine: true}, 'setENSName', 'bleeps.eth');
+      const newNode = await read('ReverseRegistrar', 'node', Bleeps.address);
+      log({ens_reverse_node: newNode});
+    } else {
+      log(`node already set: ${expectedNode}`);
+    }
   }
 };
 export default func;
