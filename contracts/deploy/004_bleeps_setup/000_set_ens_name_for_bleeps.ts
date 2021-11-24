ccb64f57007fba3d232731917741d9e6bf0b16c9
wighawag <wighawag@gmail.com>
2021-11-24 16:11:11 +0000

    fix ens name checks

--- contracts/deploy/004_bleeps_setup/000_set_ens_name_for_bleeps.ts
+++ contracts/deploy/004_bleeps_setup/000_set_ens_name_for_bleeps.ts
@@ -3,27 +3,63 @@ import {DeployFunction} from 'hardhat-deploy/types';
 // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-explicit-any
 const namehash = require('eth-ens-namehash') as any;
 
+const ReverseDefaultResolverABI = [
+  {
+    inputs: [{internalType: 'contract ENS', name: 'ensAddr', type: 'address'}],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'ens',
+    outputs: [{internalType: 'contract ENS', name: '', type: 'address'}],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{internalType: 'bytes32', name: '', type: 'bytes32'}],
+    name: 'name',
+    outputs: [{internalType: 'string', name: '', type: 'string'}],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      {internalType: 'bytes32', name: 'node', type: 'bytes32'},
+      {internalType: 'string', name: '_name', type: 'string'},
+    ],
+    name: 'setName',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments} = hre;
+  const {deployments, ethers} = hre;
   const {execute, read, log} = deployments;
 
   const Bleeps = await deployments.get('Bleeps');
-
-  const bleepsOwner = await read('Bleeps', 'owner');
-
   const ENS = await deployments.getOrNull('ENS');
 
   if (ENS) {
-    // defaultResolver TODO
-    // name
-    const expectedNode = namehash.hash(`${Bleeps.address.toLowerCase().slice(2)}.addr.reverse`);
+    const newName = 'bleeps.eth';
+    const bleepsOwner = await read('Bleeps', 'owner');
+    const defaultResolver = await read('ReverseRegistrar', 'defaultResolver');
+    const defaultResolverContract = await ethers.getContractAt(ReverseDefaultResolverABI, defaultResolver);
     const node = await read('ReverseRegistrar', 'node', Bleeps.address);
-    if (expectedNode !== node) {
-      await execute('Bleeps', {from: bleepsOwner, log: true, autoMine: true}, 'setENSName', 'bleeps.eth');
-      const newNode = await read('ReverseRegistrar', 'node', Bleeps.address);
-      log({ens_reverse_node: newNode});
+    const name = await defaultResolverContract.callStatic.name(node);
+    if (name !== newName) {
+      await execute('Bleeps', {from: bleepsOwner, log: true, autoMine: true}, 'setENSName', newName);
     } else {
-      log(`node already set: ${expectedNode}`);
+      log(`name already set: ${newName}`);
     }
   }
 };
