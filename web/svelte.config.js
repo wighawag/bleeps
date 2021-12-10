15b0d1e6d3825f8d5700001fb34d3dfb495ad8dd
wighawag <wighawag@gmail.com>
2021-12-10 09:30:48 +0000

    new build

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,9 +1,14 @@
 import preprocess from 'svelte-preprocess';
 import adapter_ipfs from 'sveltejs-adapter-ipfs';
+import {execSync} from 'child_process';
+
+const VERSION = execSync('git rev-parse --short HEAD').toString().trim();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: preprocess({
+    sourceMap: true,
+  }),
 
   kit: {
     adapter: adapter_ipfs({
@@ -13,6 +18,14 @@ const config = {
     }),
     target: '#svelte',
     trailingSlash: 'ignore',
+    vite: {
+      build: {
+        sourcemap: true,
+      },
+      define: {
+        __VERSION__: JSON.stringify(VERSION),
+      },
+    },
   },
 };
 
